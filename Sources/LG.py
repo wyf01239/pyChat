a98d9a2f1965257444f6681dd737e667ad1d4795
wyf9 <wyf01239@163.com>
2023-03-22 13:38:09 +0800

    add .gitignore && Update LG
    嗯哼哼啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊

--- Sources/LG.py
+++ Sources/LG.py
@@ -11,13 +11,13 @@ def wMain():
     while int(Over) < 15:
         Over=input('输入最高值 (大于 15 的整数): ')
         if Over == "/q":
-            print ("已退出游戏.")
+            print ("已退出游戏。")
             return 0
         try:
             if int(Over) == int(114514):
                 pass
         except:
-            print("ERROR: 非法的数字 - NaN\n请重新输入.")
+            print("ERROR: 非法的数字 - NaN\n请重新输入。")
             Over = 0
             continue
         if int(Over) < 15:
@@ -25,52 +25,55 @@ def wMain():
             a114 = a114 + 1
             if a114 > 4:
                 w114514.wMain()
-            print("ERROR: 数字太小了, 请重新输入.")
+            print("ERROR: 数字太小了, 请重新输入。")
 
     SN=random.randint(1, int(Over))
 
     go = True
     UN = "0"
+    Att = 0
     # 判定
     
     while go == True:
         try:
             if UN == "/q":
-                print ("已退出游戏.")
+                print ("已退出游戏。")
                 go = False
                 break
             elif int(UN) == 0:
                 pass 
         except:
-            print("ERROR: 非法的数字 - NaN, 请重新输入.")
+            print("ERROR: 非法的数字 - NaN, 请重新输入。")
             UN = "0"
             continue
         if int(UN) == 0:
             UN=input('请输入你的数: ')
+            Att = Att + 1
             if UN == "/q":
-                print ("已退出游戏.")
+                print ("已退出游戏。")
                 go = False
                 break
         elif int(UN) < int(SN):
             UN=input('数字小了\n请输入你的数字: ')
+            Att = Att + 1
             if UN == "/q":
-                print("已退出游戏.")
+                print("已退出游戏。")
                 return 0
         elif int(UN) > int(SN):
             UN=input('数字大了\n请输入你的数字: ')
+            Att = Att + 1
             if UN == "/q":
-                print("已退出游戏.")
+                print("已退出游戏。")
                 return 0
         elif int(UN) == int(SN):
-            UN=input('你猜对了! 数字就是: ' + str(SN) + '. 按回车继续...')
+            Att = Att + 1
+            UN=input('你猜对了! 数字就是: ' + str(SN) +  '\n你猜了: ' + str(Att) + '次！\n按回车继续...')
             go = False
             break
         else:
             print("Dead ERROR: Unknown Error. :(((((((((")
-            print("已退出游戏.")
+            print("已退出游戏。")
             go = False
             break
-
-# √  :)
-
     return 0
+# √  :)
