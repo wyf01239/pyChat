1c17201f77063a4aade26108efa14f05a5f7ab98
wyf9 <wyf01239@163.com>
2023-06-22 17:46:33 +0800

    ...

--- wFastRunDev.bat
+++ wFastRunDev.bat
@@ -1,20 +1,20 @@
 @echo off
-echo FastRunDev %1 %2 %3 %4 %5 %6 %7 %8 %9
 if "%1" == "/w" (
     %2 %3 %4 %5 %6 %7 %8 %9
     echo.
-    echo Program Exited.
-    echo Press any key x2 to Quit.
+    echo Press any key to Quit.
     pause>nul
-    echo Press any key x1 to Quit.
-    pause>nul
-    exit
+    exit %ERRORLEVEL%
 ) else (
     if "%COMPUTERNAME%" == "WYF9" (
         set wpy=python
     ) else (
         set wpy=d:\wyf9\python311\python.exe
         )
-    echo ComputerName ^= %COMPUTERNAME%
-    start %0 /w %wpy% Main.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+    set execute=Main.py
+    echo Computer Name: %COMPUTERNAME%
+    echo START %wpy% %execute% %1 %2 %3 %4 %5 %6
+    start /WAIT %0 /w %wpy% %execute% %1 %2 %3 %4 %5 %6
+    echo Process Exited.
+    echo Code: %ERRORLEVEL%
     )
